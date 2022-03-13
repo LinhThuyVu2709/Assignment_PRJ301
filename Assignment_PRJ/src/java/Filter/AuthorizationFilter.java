5d2bc4ec456e9eee54379c75d2b8fb89113fb9af
LinhThuyVu2709 <100285956+LinhThuyVu2709@users.noreply.github.com>
2022-03-13 16:51:50 +0700

    Adding filter authenticate login activity

---
+++ Assignment_PRJ/src/java/Filter/AuthorizationFilter.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Filter;
+
+import dao.AccountDAO;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.Account;
+
+/**
+ *
+ * @author LinhVT
+ */
+@WebFilter(filterName = "AuthorizationFilter", urlPatterns = {"/admin/*"})
+public class AuthorizationFilter implements Filter {
+    
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        
+
+    }
+    /**
+     * Destroy method for this filter
+     */
+    @Override
+    public void destroy() {        
+    }
+
+    /**
+     * Init method for this filter
+     */
+    @Override
+    public void init(FilterConfig filterConfig) {        
+        
+    }
+
+    /**
+     * Return a String representation of this object.
+     */
+   
+    
+}
